873e76019db96420ee1b1f426197be881644c0a0
Eternalrivers <158377388+Eternalrivers@users.noreply.github.com>
2025-03-28 12:07:46 +0800

    feat: add method def for playing board
    Use of array to make separators and the use of puts
    to have each separator in a new line.

---
+++ main.rb
@@ -0,0 +1,12 @@
+def playing_board
+  grid = []
+  arr = [%w[1 2 3], %w[4 5 6], %w[7 8 9]]
+  width = 3
+  separator_row = Array.new(width + 1, '+').join('-') << "\n"
+  cell_row = arr.map { |num| num.join('|').prepend('|') << '|' }
+
+  cell_row.each do |num|
+    grid << (separator_row + num)
+  end
+  puts grid.append(separator_row)
+end
