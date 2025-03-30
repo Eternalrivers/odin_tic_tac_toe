6740d7ca461315bf63cd8e9b9e256ec6cb462c17
Eternalrivers <158377388+Eternalrivers@users.noreply.github.com>
2025-03-30 22:52:28 +0800

    fix: undefined local variable inside class Player

--- lib/player.rb
+++ lib/player.rb
@@ -1,5 +1,7 @@
 # For creating players in a game
 class Player
+  attr_reader :name, :symbol
+
   def initialize(name, symbol)
     @name = name
     @symbol = symbol
@@ -8,6 +10,12 @@ class Player
   def self.create_player1
     name = 'player1'
     symbol = 'X'
+    Player.new(name, symbol)
+  end
+
+  def self.create_player2
+    name = 'player2'
+    symbol = 'O'
     Player.new(name, symbol)
   end
 end
