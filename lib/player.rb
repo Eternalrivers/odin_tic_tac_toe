a0de11970011a32436a80d7efa0b6552137b974c
Eternalrivers <158377388+Eternalrivers@users.noreply.github.com>
2025-03-31 08:40:02 +0800

    feat: move selection from class Board
    Since selection was a part for the player process, the method for selecting
    the slot of the board was move to the class Player. The method was adjusted
    so that it will only accept digits and adding conditions for checking if
    the slot is still available or not.
    
    Further optimization is needed and if there is a need to move the process
    through which the slots is still available or not as each player object
    contains different states.

--- lib/player.rb
+++ lib/player.rb
@@ -1,10 +1,11 @@
 # For creating players in a game
 class Player
-  attr_reader :name, :symbol
+  attr_reader :name, :symbol, :choice, :player_choice
 
   def initialize(name, symbol)
     @name = name
     @symbol = symbol
+    @player_choice = []
   end
 
   def self.create_player1
@@ -17,5 +18,16 @@ class Player
     name = 'player2'
     symbol = 'O'
     Player.new(name, symbol)
+  end
+
+  def player_selection
+    puts 'Pick a number designating the slot chosen'
+    while @slot.nil? || (@slot == []) || @player_choice.include?(@slot.to_s)
+      @slot = gets.match(/\d/)
+      puts 'Try Again'
+    end
+    @choice = @slot.to_s
+    @player_choice << @choice
+    @slot = []
   end
 end
