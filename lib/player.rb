9ac2c2658b80e7ba7c84be4402e7d89043e81b0f
Eternalrivers <158377388+Eternalrivers@users.noreply.github.com>
2025-04-02 18:22:56 +0800

    delete: remove class method for creating player
    While players differ in states, they have the same
    behaviors so it would be wise to remove the methods
    to have a cleaner code.

--- lib/player.rb
+++ lib/player.rb
@@ -1,23 +1,12 @@
 # For creating players in a game
 class Player
-  attr_reader :name, :symbol, :choice, :player_choice
+  attr_reader :name, :symbol, :choice
+  attr_accessor :player_choice
 
   def initialize(name, symbol)
     @name = name
     @symbol = symbol
     @player_choice = []
-  end
-
-  def self.create_player1
-    name = 'player1'
-    symbol = 'X'
-    Player.new(name, symbol)
-  end
-
-  def self.create_player2
-    name = 'player2'
-    symbol = 'O'
-    Player.new(name, symbol)
   end
 
   def player_selection
