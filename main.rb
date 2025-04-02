0924f0a510053ba3b82a8b4a303ab32726220aab
Eternalrivers <158377388+Eternalrivers@users.noreply.github.com>
2025-04-02 18:30:24 +0800

    feat: add method to check for line completion
    Adds method that checks if players already won the game. Player's choices
    are compared to a different array. Where the player will win if the
    combination of the player's choices is included in the array.

--- main.rb
+++ main.rb
@@ -1,4 +1,25 @@
 require_relative 'lib/board'
+require_relative 'lib/player'
 
-board = Board.new
-board.create_board
+require 'pry-byebug'
+
+@lines = [%w[1 2 3], %w[4 5 6], %w[7 8 9], %w[1 4 7], %w[2 5 8], %w[3 6 9], %w[1 5 9], %w[3 5 7]]
+
+@game_won = false
+
+def check_lines?(player_choices)
+  return false if player_choices.empty?
+
+  player_choices.combination(3) do |arr|
+    @game_won = true if @lines.include?(arr)
+  end
+end
+
+@board = Board.new
+puts @board.create_board
+
+@player1 = Player.new('Player_1', 'X')
+@player2 = Player.new('Player_2', 'O')
+
+player1_slots = @player1.player_choice
+player2_slots = @player2.player_choice
