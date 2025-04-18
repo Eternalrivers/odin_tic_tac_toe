21f22be9bab44e08b282105064c784e396b80c3f
Eternalrivers <158377388+Eternalrivers@users.noreply.github.com>
2025-04-18 16:55:42 +0800

    feat: add method for switching players

--- lib/game.rb
+++ lib/game.rb
@@ -4,11 +4,23 @@ class Game
 
   def initialize(player1, player2)
     @board = Board.new
-    @player1 = player1.new('Player_1', 'X')
-    @player2 = player2.new('Player_2', 'O')
+    @current_player = 0
+    @player = [player1.new('Player_1', 'X'), player2.new('Player_2', 'O')]
     @game_won = false
     @lines = [%w[1 2 3], %w[4 5 6], %w[7 8 9], %w[1 4 7], %w[2 5 8], %w[3 6 9],
               %w[1 5 9], %w[3 5 7]]
+  end
+
+  def other_player
+    1 - @current_player
+  end
+
+  def switch_players
+    @current_player = other_player
+  end
+
+  def current_player
+    @player[@current_player]
   end
 
   def check_lines?(player_choices)
@@ -29,21 +41,16 @@ class Game
 
   def play
     i = 0
-    player1_slots = @player1.player_choice
-    player2_slots = @player2.player_choice
     puts @board.create_board
 
     until @game_won == true
-      game_turn(@board, @player1)
-      check_lines?(player1_slots.sort)
+      game_turn(@board, current_player)
+      check_lines?(current_player.player_choice.sort)
       puts 'Game Over! Player 1 won the game!' if @game_won == true
       i += 1
       break if @game_won == true || i == 9
 
-      game_turn(@board, @player2)
-      check_lines?(player2_slots.sort)
-      puts 'Game Over! Player 2 won the game!' if @game_won == true
-      i += 1
+      switch_players
     end
     puts "Game Over! It's a draw!" if @game_won == false
   end
